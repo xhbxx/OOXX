3acbbf64d709360aadc8c52116da901c760d64e2
xhbxx <120081648+xhbxx@users.noreply.github.com>
2026-03-11 12:32:02 +0800

    ing

---
+++ 井字棋/ai.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+class AI
+{
+
+};
---
+++ 井字棋/board.h
@@ -0,0 +1,7 @@
+#pragma 
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+extern int board[3][3];
