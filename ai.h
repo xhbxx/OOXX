4d8dbd8b05049ef171f069bc6a825eedb80ee236
xhbxx <120081648+xhbxx@users.noreply.github.com>
2026-03-14 21:23:24 +0800

    更新

--- ai.h
+++ ai.h
@@ -5,24 +5,24 @@ class ai
 {
 public:
 	ai(int color);
+	int aiColor;
+	//价值数组 
+	vector<double> value;
+	//随机率 
+	double Epsilon;
+	//学习率 
+	double alpha;
+	//记录自己走后的局面 
+	int stored_OutCome;
 	//重置储存的outcome
 	void reset();
 	int move();
-	//价值数组 
-	vector<double> value;
-	int aiColor;
-	//随机率 
-	double Epsilon;
 	int minmax(int Color, int depth);
 	//转化棋盘 
 	int convertBoard(int Board[9]);
 	void revertBoard(int index,int board[9]);
 	//生成随机数 
-	int random(int n);
-	//学习率 
-	double alpha;
-	//记录自己走后的局面 
-	int stored_OutCome;
+	int random(int n);		
 };
 //获取价值表 
 void saveValueTable(const std::vector<double>& table, const std::string& filename);
